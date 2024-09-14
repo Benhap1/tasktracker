d5bc6389dc5a68a0da05f48295ec6f49a9c7418c
Bakhtiyar <bahtiyar.bobokulov1@gmail.com>
2024-09-14 14:19:28 +0300

    6 commit - Authorize in Swagger added

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -11,14 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info().title("Task Tracker API")
-//                        .version("1.0")
-//                        .description("API для управления задачами и пользователями"));
-//    }
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
